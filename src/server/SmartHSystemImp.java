0bf5d362ea020a243d62b6971309c8525b0061ed
Rikkokiri <pilvi.rajala@gmail.com>
2016-03-11 00:38:43 +0200

    Methods for modifying users partially added

--- src/server/SmartHSystemImp.java
+++ src/server/SmartHSystemImp.java
@@ -3,6 +3,8 @@ package server;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import model.UserAccountHandler;
+
 /**
  * The purpose of 
  * @author Pilvi
@@ -11,12 +13,32 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class SmartHSystemImp extends UnicastRemoteObject implements SmartHSystem {
 
+	private UserAccountHandler userHandler;
+	
+	
 	public SmartHSystemImp() throws RemoteException {
 		super();
 		
 		//TODO
 	
+		//Create user account handler
+		userHandler = new UserAccountHandler();
+		
 	} //constructor
+
+
+	
+	// • • • • • • • • USERS • • • • • • • • •
+	
+	public void newUser(String username, String password) throws RemoteException {
+		// TODO Auto-generated method stub
+	}
+
+	public void deleteUser(String username) throws RemoteException {
+		// TODO Auto-generated method stub
+	}
+	
+	
 	
 	
 	
