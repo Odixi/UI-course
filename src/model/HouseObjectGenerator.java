fc9c7a7c21ea8e4a1b77429c970fdf949e45e238
Rikkokiri <pilvi.rajala@gmail.com>
2016-03-13 19:58:22 +0200

    XSD for houses finally basically done, XML on the way.

---
+++ src/model/HouseObjectGenerator.java
@@ -0,0 +1,13 @@
+package model;
+
+public class HouseObjectGenerator extends XMLHandler {
+
+//	private static final String housefilepath = "src/xmlfiles/"
+	
+	public static void main(String args[]){
+		
+	}
+	
+	
+	
+}
