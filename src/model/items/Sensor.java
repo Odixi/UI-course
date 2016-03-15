aceaf84dbb17252a7c1bdf2604ef3f446341c3b5
Rikkokiri <pilvi.rajala@gmail.com>
2016-03-15 21:42:51 +0200

    SensorType & SensorUnit items created. Sensor class itself pretty much ready. Only min & max values for different units needed.

--- src/model/items/Sensor.java
+++ src/model/items/Sensor.java
@@ -2,8 +2,60 @@ package model.items;
 
 public class Sensor extends SmartItem {
 
-	public Sensor(){
+	private SensorType sensorType;
+	private SensorUnit sensorUnit;
+	private double sensorValue;
+	
+	private double maxValue;	//This have to be defined for different units.
+	private double minValue;
+	
+	public Sensor(SensorType type){
+		this.sensorType = type;
 		
+		switch (sensorType) {
+			case TEMPERATURE:
+				sensorUnit = SensorUnit.CELCIUS;
+				//Set maxValue & minValue
+			case HUMIDITY:
+				sensorUnit = SensorUnit.HUMIDITYPERCENT;
+			case LIGHT:
+				sensorUnit = SensorUnit.LUMEN;
+		}	
+		
+		//Set default values;
+		
+	} //constructor
+
+	
+	//--------- SENSOR TYPE & MEASURING UNIT ----------------
+	
+	public SensorType getSensorType() {
+		return sensorType;
+	}
+
+	public SensorUnit getSensorUnit() {
+		return sensorUnit;
+	}
+
+	//----------------- SENSOR VALUE --------------------------------
+	
+	public double getSensorValue(){
+		return sensorValue;
+	}
+
+	
+	/**
+	 * Returns true if the value was between set min & max (for the measuring unit) and value was therefore set successfully.
+	 * @param newValue
+	 * @return
+	 */
+	public boolean setValue(double newValue){
+		if(newValue <= maxValue && newValue >= minValue){
+			sensorValue = newValue;
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 }
---
+++ src/model/items/SensorUnit.java
@@ -0,0 +1,10 @@
+package model.items;
+
+public enum SensorUnit {
+	CELCIUS,
+	HUMIDITYPERCENT,
+	LUMEN
+	
+	//Come up with more if you want to.
+	
+}
